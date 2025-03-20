8c57e4d177d1a62888a898abfbe58f58212f7777
tierik-bjornson <bjornson25102003@gmail.com>
2025-03-20 15:06:23 +0700

    angular commit

--- api/src/main/java/com/manageleaguefootball/demo/dto/Info/TeamPageInfo.java
+++
@@ -1,9 +0,0 @@
-package com.manageleaguefootball.demo.dto.Info;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class TeamPageInfo extends PageInfo {
-}
